9434298301f9797fd925a131e09818ef83d43dd4
Anatolii Sakhnik <sakhnik@gmail.com>
2015-05-27 23:32:21 +0300

    Get rid of socat requirement

--- cmake/Modules/FindSocat.cmake
+++
@@ -1,7 +0,0 @@
-find_program (Socat_PROGRAM socat)
-
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (Socat  DEFAULT_MSG
-  Socat_PROGRAM)
-
-mark_as_advanced (Socat_PROGRAM)
